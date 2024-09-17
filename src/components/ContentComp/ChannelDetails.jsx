e88f9fbe7e76d79d9e362b151be7cf8a9c446884
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-09-17 22:58:03 +0530

    refactor: Improve channel details layout and add avatar and buttons

--- src/components/ContentComp/ChannelDetails.jsx
+++ src/components/ContentComp/ChannelDetails.jsx
@@ -2,7 +2,26 @@ const UserDetails = ({userData}) => {
     return (
        <div className="channel-details-main-content">
             <div className="channel-details">
-                <img src={userData[2]} alt="Avatar" />
+                <img src={userData[3]} alt="coverImager" className="channel-details-cover" />
+                <div className="channel-details-info">
+                    <div className="channel-details-avatar">
+                        <img src={userData[2]} alt="Avatar" />
+                    </div>
+                    <div className="channel-details-info-about">
+                        <h1 className="channel-details-info-about-fullname">{userData[0]}</h1>
+                        <p className="channel-details-info-about-username">@{userData[1]}  *0 subscriber</p>
+                        <p style={{color:'rgb(159, 158, 158)'}}>More about this channel ...<span style={{color:'white'}}>more</span></p>
+
+                        <div className="channel-details-info-about-buttons">
+                            <div className="customise-channel">
+                                Customise Channel
+                            </div>
+                            <div className="manage-videos">
+                                Manage Videos
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="channel-details-options">
                 PLAYLIST
